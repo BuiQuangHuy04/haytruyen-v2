ea43a60d06a5f5073af637b611a5bcb0062b8b77
Bui Huy <74649569+BuiQuangHuy04@users.noreply.github.com>
2022-04-23 14:20:12 +0700

    Update HomeController.java

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -20,10 +20,7 @@ public class HomeController implements IController {
         String by = null;
         String value = null;
         String text = null;
-
-        boolean showCarousel = true;
-        boolean showBreadcrumb = true;
-
+        
         //tao link truyen
         if (request.getParameter("text") != null) {
             text = request.getParameter("text").trim();
@@ -38,20 +35,8 @@ public class HomeController implements IController {
             value = request.getParameter("value").trim();
             url = url + "&value=" + value;
         }
-        //tao breadcrumb
-        if (by != null || text != null) {  //Filter
-            showCarousel = false;
-            if (by != null)
-                ctx.setVariable("breadCrumb", value);
-            else if (text != null)
-                ctx.setVariable("breadCrumb", "Search result for: <b>" + text + "</b>");
-        } else { //Home
-            showBreadcrumb = false;
-        }
-        //tra ve link truyen, breadcrumb de dung o ben front end
+        //tra ve link truyen de dung o ben front end
         ctx.setVariable("url", url);
-        ctx.setVariable("showCarousel", showCarousel);
-        ctx.setVariable("showBreadcrumb", showBreadcrumb);
         //phan trang
         long totalPages = new MangaService().getTotalPages(by, value, text);
         ctx.setVariable("totalPages", totalPages);
