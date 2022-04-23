6b9571e08558414df82a879a0d15fae6c6f9e10a
Bui Huy <74649569+BuiQuangHuy04@users.noreply.github.com>
2022-04-23 14:44:17 +0700

    Update MangaService.java

--- src/main/java/service/MangaService.java
+++ src/main/java/service/MangaService.java
@@ -1,35 +1,12 @@
 package service;
 
 import DAO.MangaDAO;
-import model.Manga;
 import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MangaService {
-    
-    //moi trang mac dinh co 12 truyen
+
+    //moi trang mac dinh co 4 truyen
     final static int NUM_OF_MANGA_ON_PAGE = 4;
-
-    //ham tim kiem truyen
-    public List<Manga> searchManga(String by, String value, int page, String text) {
-        Document filter = new Document();
-        Document sort = new Document();
-
-        if (by != null && value != null)
-            filter.append(by, value);
-        if (text != null)
-            filter.append("$text", new Document("$search", text));
-        else
-            sort.append("mangaName", 1);
-
-        List<Manga> list = new MangaDAO().searchManga(filter, sort, NUM_OF_MANGA_ON_PAGE, (page - 1) * NUM_OF_MANGA_ON_PAGE);
-        if (list == null) {
-            list = new ArrayList<>();
-        }
-        return list;
-    }
 
     //tinh so trang
     public long getTotalPages(String by, String value, String text) {
